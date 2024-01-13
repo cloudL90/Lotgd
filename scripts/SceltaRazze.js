7b727837326c0dc72f54c882dd804c84337593ec
cloudL90 <clicari3@gmail.com>
2024-01-13 22:34:18 +0100

    prima integrazione col BE

--- scripts/SceltaRazze.js
+++ scripts/SceltaRazze.js
@@ -2,11 +2,11 @@ const ul = document.querySelector("ul");
 const br = document.createElement("br");
 
 async function GetRaces() {
-    const response = await fetch("http://localhost:3000/Races");
+    const response = await fetch("http://localhost:5126/api/Races");
     const races = await response.json();
     for (const race of races) {   
         const li = document.createElement("li");
-        li.innerHTML = race;
+        li.innerHTML = race.Name;
         ul.append(li);
     }
 };
